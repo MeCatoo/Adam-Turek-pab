5aa662c118b4e1677269f71fae4536aaef941279
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-03-09 11:33:52 +0100

    ts koniec zajec

--- Projekt1/index.ts
+++ Projekt1/index.ts
@@ -1,7 +1,32 @@
 const express = require('express')
 const app = express()
-app.get('/:operation/:num1/:num2', function (req, res)
-{ 
-    res.send(req.querry.operation)
- })
+app.get('/:operation/:num1/:num2', function (req, res) {
+    var num1 = parseFloat(req.params.num1)
+    var num2 = parseFloat(req.params.num2)
+    if(req.params.operation == "dodaj" || req.params.operation == "add")
+    {
+        res.send("Wynik: "+ (num1 + num2))
+    }
+    else if(req.params.operation == "odejmij" || req.params.operation == "subtract")
+    {
+        res.send("Wynik: "+ (num1 - num2))
+    }
+    else if(req.params.operation == "pomnoz" || req.params.operation == "times")
+    {
+        res.send("Wynik: "+ (num1 * num2))
+    }
+    else if(req.params.operation == "podziel" || req.params.operation == "divide")
+    {
+        if(num2!=0)
+        {
+            res.send("Wynik: "+ (num1 / num2))
+        }
+        else{
+            res.send("Error")
+        }
+    }
+    else{
+        res.send("Error")
+    }
+});
  app.listen(3000)
