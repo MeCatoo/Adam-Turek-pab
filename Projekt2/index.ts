c8bc2215fb49870dc82b516c3087ed687e507226
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-04-06 10:35:28 +0200

    Login v1

--- Projekt2/index.ts
+++ Projekt2/index.ts
@@ -5,10 +5,12 @@ import { normalize } from 'path'
 import { Note } from './note'
 import { Tag } from './tag'
 import fs from 'fs';
-import { json } from 'stream/consumers'
+import json from 'stream/consumers'
+import jwt from 'jsonwebtoken';
+
 
 const app = express()
-const storeFile = "Projekt2\node_modules\Storage.json"
+const storeFile = "Storage.json"
 let notes: Note[] = []
 let tags: Tag[] = [];
 readStorage();
@@ -27,26 +29,47 @@ readStorage();
 //     }))
 app.use(express.json())
 
+app.post('/login', function (req: Request, res: Response) {
+  if (req.body.login && req.body.password) {
+    if (req.body.login === "123" && req.body.password === "123") {
+      res.status(200).send(jwt.sign("payload", "secret"))
+    }
+    else
+      res.status(401).send("błędne hasło lub login")
+  }
+  else
+    res.status(401).send("podaj login i hasło")
+})
+
 app.get('/note/:id', function (req: Request, res: Response) {
+  if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
   const note = FindById(+req.params.id)
   note ?? res.send(404)
   res.status(200).send(note)
 })
 app.get('/note', function (req: Request, res: Response) {
+  if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
   res.status(200).send(notes)
 })
 
 app.get('/tag/:id', function (req: Request, res: Response) {
+  if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
   const tag = FindTagById(+req.params.id)
   tag ?? res.send(404)
   res.status(200).send(tag)
 })
 app.get('/tag', function (req: Request, res: Response) {
+  if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
   res.status(200).send(tags)
 })
 
 app.post('/note', function (req: Request, res: Response) {
-
+  if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
   const tagsTmp: Tag[] = [];
   req.body.tags.forEach((element: Tag) => {
     tagsTmp.push(FindTagByName(element.name))
@@ -73,6 +96,8 @@ app.post('/note', function (req: Request, res: Response) {
 
 app.post('/tag',
   function (req: Request, res: Response) {
+    if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
     if (req.body.name) {
       const tag = CreateTag(req.body.name)
       updateStorage()
@@ -84,6 +109,8 @@ app.post('/tag',
 
 app.put('/note/:id',
   function (req: Request, res: Response) {
+    if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
     const note = FindById(+req.params.id)
     if (note) {
       const editedNote: Note = new Note({
@@ -103,6 +130,8 @@ app.put('/note/:id',
 
 app.put('/tag/:id',
   function (req: Request, res: Response) {
+    if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
     const tag = FindTagById(+req.params.id)
     if (tag) {
       tag.name = req.body.name
@@ -115,6 +144,8 @@ app.put('/tag/:id',
 
 app.delete('/note/:id',
   function (req: Request, res: Response) {
+    if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
     if (FindById(+req.params.id)) {
       notes.splice(FindIndexById(+req.params.id), 1)
       res.status(200).send("Usunięto")
@@ -126,6 +157,8 @@ app.delete('/note/:id',
 
 app.delete('/tag/:id',
   function (req: Request, res: Response) {
+    if(!CheckToken(req.headers.authorization))
+  return res.status(401).send("wymagane logowanie")
     if (FindTagById(+req.params.id)) {
       tags.splice(FindTagIndexById(+req.params.id), 1)
       res.status(200).send("Usunięto")
@@ -136,6 +169,7 @@ app.delete('/tag/:id',
 
 
 function FindById(id: number) {
+  
   const note = notes.find(function (note: Note): boolean {
     if (note.id === id) {
       return true
@@ -221,6 +255,17 @@ async function readStorage(): Promise<void> {
   } catch (err) {
     console.log(err)
   }
+}
+function CheckToken(token: any): boolean {
+  if (token) {
+    const tmp = token.split(" ", 2)
+    if (tmp[1] === "eyJhbGciOiJIUzI1NiJ9.cGF5bG9hZA.4GMt2k_zZryxhKgC8_HvdSZtYxyEyDa0AFIL-n60a8M")
+      return true
+    else
+      return false
+  }
+  else
+    return false
 }
 app.listen(3000)
 
