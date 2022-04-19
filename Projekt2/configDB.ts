5fde2c98d4b69dd739a6aea34e076b2c9bc0d72e
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-04-20 00:37:05 +0200

    mongoDB

---
+++ Projekt2/configDB.ts
@@ -0,0 +1,20 @@
+//import mongoose from 'mongoose'
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb+srv://username:password@cluster0.hlmgs.mongodb.net/pab?retryWrites=true&w=majority')
+
+const UserSchema = new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      default: 0,
+    },
+  });
+  
+  const User = mongoose.model("User", UserSchema);
+// mongoose.connect('mongodb://username:password@cluster0.hlmgs.mongodb.net:27017/pab');
+//mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]
+
