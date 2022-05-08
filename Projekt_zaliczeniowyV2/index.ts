5ac2c7e6c586a95d626f65bbcda51d2e710a72aa
Adam Turek <adam.turek@microsoft.wsei.edu.pl>
2022-05-08 21:43:01 +0200

    new classes

--- Projekt_zaliczeniowyV2/index.ts
+++ Projekt_zaliczeniowyV2/index.ts
@@ -2,7 +2,12 @@ import { Pracownik } from "./lib/pracownik";
 import { Restauracja } from "./lib/restauracja";
 import { Rezerwacja} from "./lib/rezerwacja"
 import { Status, Stolik } from "./lib/Stolik";
+import { Zamowienie } from "./lib/zamowienie";
+import { Danie, Kategoria } from "./lib/danie";
+
 
 console.log(new Restauracja("Nazwa", "Zacny adres"))
 console.log(new Pracownik("Imie", "Nazwisko"))
 console.log(new Rezerwacja(new Stolik("nazwa",1, Status.wolny)))
+const dania = [new Danie("Paróweczki", 10, Kategoria.danie_główne)]
+console.log(new Zamowienie(dania,new Stolik("asdasd", 1), new Pracownik("Imie", "Nazwisko")))
